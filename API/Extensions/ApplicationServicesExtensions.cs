68913c955197dc12b34c34264e73baefb40d77ac
ahmad binmana <ahmadam37@hotmail.com>
2025-02-10 15:03:44 +0300

    adding Application Services Extensions

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection ApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+
+        services.AddControllers();
+       services.AddDbContext<DataContext>(opt =>
+        {
+
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenServices>();
+
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -9,14 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>{
 
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService , TokenServices>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
 
     var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token not found");
