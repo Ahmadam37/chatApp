1391e29cfd581d257eacb3c9f0a4928179cf160b
ahmad binmana <ahmadam37@hotmail.com>
2025-02-18 18:00:14 +0300

    Using the service to store state

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -11,17 +11,15 @@ import { MemberCardComponent } from "../member-card/member-card.component";
   styleUrl: './members-list.component.css'
 })
 export class MembersListComponent implements OnInit{
-  private memberService = inject(MembersService);
+   memberService = inject(MembersService);
   members: Member [] = [];
 
   ngOnInit(): void {
-    this.loadMembers()
+    if(this.memberService.members().length === 0) this.loadMembers();
   }
 
   loadMembers(){
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.memberService.getMembers()
   }
 
 }
